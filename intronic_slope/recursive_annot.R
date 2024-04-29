1833a56ff7c48458c5327f2454d41469a317f0bc
PapadakisAntonis <60514101+PapadakisAntonis@users.noreply.github.com>
2024-04-29 15:04:47 +0200

    Update recursive_annot.R

--- intronic_slope/recursive_annot.R
+++ intronic_slope/recursive_annot.R
@@ -1,48 +1,50 @@
-#Script to find recursively spliced introns
+#Script to find introns with sufficient split reads bridging the intron.
 
 library(plyr)
 library(data.table)
 
+#Arguments for the location of the splice junction output files. Can be changed according to the f
 args = commandArgs(trailingOnly=TRUE)
 if (length(args)==0) {
   stop("At least one argument must be supplied (input file).n", call.=FALSE)
 } else if (length(args)==1) {
   # default output file
-  args[2] = "mmusculus"
+  args[1] = "mmusculus"
 }
-spe=args[1]
-s=args[2]
+spe="celegans"
+s="celegans"
 
+#Function to filter out splcie junctions without enough coverage.
 filter <- function(x, rsize=10, nb=4)
 {
-    return(x[((x$V9 >= rsize) & (x$V7 >= nb)),])
+  return(x[((x$V9 >= rsize) & (x$V7 >= nb)),])
 }
 
-filepath <- paste(spe, "/*SJ.out.tab", sep="") #splice junction output of STAR alignment
+filepath <- paste("/cellfile/datapublic/cdebes/cdebes/workspace/scripts/",spe, "/*SJ.out.tab", sep="") #splice junction output of STAR alignment
 cov <- Sys.glob(filepath)
 splice <- lapply(1:length(cov), function(x)fread(cov[x], header=F))
-splice <- lapply(splice, filter, 20, 6)
+splice <- lapply(splice, filter, 20, 4)
 splice <- do.call(rbind, splice)
 splice <- cbind(splice, n=rep(1, nrow(splice)))
 complete <- splice[, lapply(.SD, sum), by = list(V1, V2, V3, V4, V5, V6)]
 complete <- complete[complete$n >= length(cov),]
 
-write.table(complete, file=paste("/data/public/cdebes/workspace/scripts/", spe ,"/SJ.complete.bed", sep=""), quote=FALSE, row.names=FALSE, col.names=FALSE)
+write.table(complete, file=paste("/cellfile/datapublic/cdebes/cdebes/workspace/scripts/", spe ,"/SJ.complete.bed", sep=""), quote=FALSE, row.names=FALSE, col.names=FALSE)
 
-system(paste("cat /data/public/cdebes/workspace/scripts/",spe,"/SJ.complete.bed | awk '{OFS=\"\t\"} {{if ($4 == 1) {print $1, $2, $3, \"+\", $5} else if ($4 == 2) {print $1, $2, $3, \"-\", $5}}}' | bedtools intersect -wa -wb -a stdin -b /data/public/cdebes/workspace/genomes/",s,"/intron_junction_ensembl.bed > /data/public/cdebes/workspace/scripts/",spe,"/junanot.bed", sep=""))
+system(paste("cat /cellfile/datapublic/cdebes/cdebes/workspace/scripts/",spe,"/SJ.complete.bed | awk '{OFS=\"\t\"} {{if ($4 == 1) {print $1, $2, $3, \"+\", $5} else if ($4 == 2) {print $1, $2, $3, \"-\", $5}}}' | bedtools intersect -wa -wb -a stdin -b /cellfile/datapublic/cdebes/cdebes/workspace/genomes/",s,"/intron_junction_ensembl.bed > /cellfile/datapublic/cdebes/cdebes/workspace/scripts/",spe,"/junanot.bed", sep=""))
 
-j <- fread(paste("/data/public/cdebes/workspace/scripts/",spe,"/junanot.bed",sep=""), header=F)
+j <- fread(paste("/cellfile/datapublic/cdebes/cdebes/workspace/scripts/",spe,"/junanot.bed",sep=""), header=F)
 j <-  as.data.frame(j)
 head(j)
 colnames(j) <- c("chrA", "startA", "endA", "strandA", "reads", "chrB", "startB", "endB", "strandB", "id", "score", "strand")
 j$length <- j$endB - j$startB
-commonStartEnd <- j[(j$strandA == j$strandB) & (j$startA-1 == j$startB) & (j$endA == j$endB),]
+commonStartEnd <- j[(j$strandA == j$strandB) & (j$startA == j$startB) & (j$endA == j$endB),]
 
-commonStart <- j[(j$strandA == j$strandB) & (j$startA-1 == j$startB) & (j$endA < j$endB),]
+commonStart <- j[(j$strandA == j$strandB) & (j$startA == j$startB) & (j$endA < j$endB),]
 
-commonEnd <- j[(j$strandA == j$strandB) & (j$startA-1 > j$startB) & (j$endA == j$endB),]
+commonEnd <- j[(j$strandA == j$strandB) & (j$startA > j$startB) & (j$endA == j$endB),]
 
-diff <- j[(j$strandA == j$strandB) & (j$startA-1 > j$startB) & (j$endA < j$endB),]
+diff <- j[(j$strandA == j$strandB) & (j$startA > j$startB) & (j$endA < j$endB),]
 
 commonStartEndNoRec <- commonStartEnd[ ! ((commonStartEnd$strandA %in% commonStart$strandA) & (commonStartEnd$startA %in% commonStart$startA)) | ((commonStartEnd$strandA %in% commonEnd$strandA) & (commonStartEnd$endB %in% commonEnd$endB)),]
 commonStartEndRec <- rbind(commonStart, commonEnd, diff)
@@ -50,5 +52,5 @@ commonStartEndRec <- rbind(commonStart, commonEnd, diff)
 head(commonStartEndNoRec[,c("chrA", "startA", "endA", "strandA",  "reads", "id", "score", "length")])
 head(commonStartEndRec[,c("chrA", "startA", "endA", "strandA",  "reads", "id", "score", "length")])
 
-write.table(commonStartEndNoRec[,c("chrA", "startA", "endA", "strandA",  "id", "score", "strandA")], file=paste("/data/public/cdebes/workspace/scripts/",spe,"/NoRecIntronJunction.bed", sep=""), sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE)
-write.table(commonStartEndRec[,c("chrA", "startA", "endA", "strandA",  "id", "score", "strandA")], file=paste("/data/public/cdebes/workspace/scripts/",spe,"/RecIntronJunction.bed", sep=""), sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE)
+write.table(commonStartEndNoRec[,c("chrA", "startA", "endA", "strandA",  "id", "score", "strandA")], file=paste("/cellfile/datapublic/cdebes/cdebes/workspace/scripts/",spe,"/NoRecIntronJunction.bed", sep=""), sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE)
+write.table(commonStartEndRec[,c("chrA", "startA", "endA", "strandA",  "id", "score", "strandA")], file=paste("/cellfile/datapublic/cdebes/cdebes/workspace/scripts/",spe,"/RecIntronJunction.bed", sep=""), sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE)
