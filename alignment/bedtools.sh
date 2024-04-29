bc0f1cb1e55a2e6fe77a8ac2b3e290cfc94f5706
PapadakisAntonis <60514101+PapadakisAntonis@users.noreply.github.com>
2024-04-29 16:05:04 +0200

    fixed bedtools.sh typo

--- alignment/bedtools.sh
+++ alignment/bedtools.sh
@@ -16,5 +16,5 @@ cat $gtf_input| awk '{OFS="\t"}{if ($3 == "five_prime_utr") {print $1,$4,$5,"5pr
 cat $gtf_input| awk '{OFS="\t"}{if ($3 == "three_prime_utr") {print $1,$4,$5,"3prim",$10,$7}}' | bedtools sort > $bed_3prim #Bed file of 3′ untranslated regions
 
 #To find the positions of the introns, we substract the exons and UTR positions from the gene coordinates. Overlapping introns are merged to remove duplicated regions from the analysis.
-cat $bed_gene  | bedtools subtract -s -a stdin -b $bed_exon | bedtools subtract -s -a stdin -b $bed_5prim | bedtools subtract -s -a stdin -b $bed_3prim" | bedtools sort | bedtools merge -i stdin -s -c 4,5,6 -o distinct > $bed intron #Bed file of introns
+cat $bed_gene  | bedtools subtract -s -a stdin -b $bed_exon | bedtools subtract -s -a stdin -b $bed_5prim | bedtools subtract -s -a stdin -b $bed_3prim | bedtools sort | bedtools merge -i stdin -s -c 4,5,6 -o distinct > $bed intron #Bed file of introns
 
